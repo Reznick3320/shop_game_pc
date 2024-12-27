c828d1303626d97457468f789914855335ef57a6
Reznick <reznick59@gmail.com>
2024-12-27 12:34:56 +0200

    commit

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+
+const openModalButton = document.getElementById("openModalButton");
+const modal = document.getElementById("modal");
+const closeButton = document.querySelector(".close-button");
+ 
+openModalButton.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+ 
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+ 
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
