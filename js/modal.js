6aed8a0600081d4ab003dce88c221875f0bdfff6
Reznick <reznick59@gmail.com>
2024-12-30 10:59:15 +0200

    commit4

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,34 @@
+document.querySelectorAll('.addToCartButton').forEach(button => {
+  button.addEventListener('click', function() {
+    const productId = this.getAttribute('data-product-id');
+    const productName = this.getAttribute('data-product-name');
+    const productPrice = parseFloat(this.getAttribute('data-product-price'));
+ 
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+ 
+    const productIndex = cart.findIndex(item => item.id === productId);
+    if (productIndex > -1) {
+      cart[productIndex].quantity += 1;  
+    } else {
+      cart.push({ 
+        id: productId, 
+        name: productName, 
+        price: productPrice, 
+        quantity: 1 
+      });  
+    }
+ 
+    localStorage.setItem('cart', JSON.stringify(cart));
+ 
+    const parentDiv = this.parentElement;
+    parentDiv.innerHTML = `
+      <button class="btn goToCartButton">Vai al carrello</button>
+    `;
+ 
+    parentDiv.querySelector('.goToCartButton').addEventListener('click', function() {
+      window.location.href = 'order.html';
+    });
 
-const openModalButton = document.getElementById("openModalButton");
-const modal = document.getElementById("modal");
-const closeButton = document.querySelector(".close-button");
- 
-openModalButton.addEventListener("click", () => {
-  modal.style.display = "block";
-});
- 
-closeButton.addEventListener("click", () => {
-  modal.style.display = "none";
-});
- 
-window.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
+    alert(`"${productName}" Aggiunto al carrello!`);
+  });
 });
